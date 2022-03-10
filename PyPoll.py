53e2a909639f42cc06aa564870d46cd17d310187
Brayden <braydendodson@gmail.com>
2022-03-10 13:00:33 -0800

    new repo structure and new pypoll

---
+++ PyPoll.py
@@ -0,0 +1,10 @@
+#data to retrive
+
+# 1. the total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. percentage of votes each candidate won
+# 4. the total number of votes each candidate won
+# 5. the winner of the election based on popular vote
+
+import csv
+dir(csv)
